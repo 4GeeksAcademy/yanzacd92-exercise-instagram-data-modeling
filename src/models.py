b920273c3a99c2161055439cf2f9561acd3df4eb
yanzacd92 <121258474+yanzacd92@users.noreply.github.com>
2023-05-12 15:06:57 +0000

    Added classes 2

--- src/models.py
+++ src/models.py
@@ -31,7 +31,6 @@ class Media(Base):
 
 class Follower(Base):
     __tablename__ = 'follower'
-    id = Column(Integer, primary_key=True)
     user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     user = relationship(User)
